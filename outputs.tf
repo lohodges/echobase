8ebdd82ce70c7049be30240b346a0b9cbe24fdfc
lohodges <226792613+lohodges@users.noreply.github.com>
2026-01-19 18:27:16 -0500

    bonus a outputs
    uncommented the bonus_a outputs

--- outputs.tf
+++ outputs.tf
@@ -30,21 +30,21 @@ output "echobase_log_group_name" {
 # added by Lonnie Hodges
 #Bonus-A outputs (append to outputs.tf)
 # Explanation: These outputs prove echobase built private hyperspace lanes (endpoints) instead of public chaos.
-# output "echobase_vpce_ssm_id" {
-#   value = aws_vpc_endpoint.echobase_vpce_ssm01.id
-# }
+output "echobase_vpce_ssm_id" {
+  value = aws_vpc_endpoint.echobase_vpce_ssm01.id
+}
 
-# output "echobase_vpce_logs_id" {
-#   value = aws_vpc_endpoint.echobase_vpce_logs01.id
-# }
+output "echobase_vpce_logs_id" {
+  value = aws_vpc_endpoint.echobase_vpce_logs01.id
+}
 
-# output "echobase_vpce_secrets_id" {
-#   value = aws_vpc_endpoint.echobase_vpce_secrets01.id
-# }
+output "echobase_vpce_secrets_id" {
+  value = aws_vpc_endpoint.echobase_vpce_secrets01.id
+}
 
-# output "echobase_vpce_s3_id" {
-#   value = aws_vpc_endpoint.echobase_vpce_s3_gw01.id
-# }
+output "echobase_vpce_s3_id" {
+  value = aws_vpc_endpoint.echobase_vpce_s3_gw01.id
+}
 
 output "echobase_private_ec2_instance_id_bonus" {
   value = aws_instance.echobase_ec201_private_bonus.id
